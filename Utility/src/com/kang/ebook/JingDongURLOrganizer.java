7862c01a5ebf7f7c48fd97c0d6a14853333ae94c
hsnyxfhyqhyh <xfyang46@gmail.com>
2014-09-19 08:30:22 -0400

    changes for grabbing bible study material

--- Utility/src/com/kang/ebook/JingDongURLOrganizer.java
+++ Utility/src/com/kang/ebook/JingDongURLOrganizer.java
@@ -2,11 +2,21 @@ package com.kang.ebook;
 
 public class JingDongURLOrganizer {
 	public static void main (String args[]){
-		String prefixUrl = "http://www.yi-see.com/read_";
-		String suffixUrl = "_12692.html";
-		int initNumber = 117893;
-		for (int i=0; i< 70; i++){
-			System.out.println("\"" + prefixUrl + (initNumber + i )+ suffixUrl + "\",");
+		String prefixUrl = "http://www.edzx.com/chajing/Old%20Testament/23Isa/23ET";
+		String suffixUrl = ".htm";
+		int initNumber = 0;
+		for (int i=0; i< 67; i++){
+			System.out.println("\"" + prefixUrl + paddingLeadingzeros (2, initNumber + i )+ suffixUrl + "\",");
 		}
+	}
+	
+	private static String paddingLeadingzeros(int length, int number) { 
+		
+		 String n = number + "";
+		 while (n.length() < length) {
+			 n = "0" + n;
+		 }
+		
+		 return n;
 	}
 }
